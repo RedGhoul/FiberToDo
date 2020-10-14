65d62afd254dc3f627aae51a95cfc89c6c5b07a6
RedGhoul <avaneesab5@gmail.com>
2020-10-13 22:33:31 -0400

    Updated a few files

--- controllers/index.go
+++ controllers/index.go
@@ -1,13 +1,11 @@
 package controllers
 
 import (
-	"github.com/RedGhoul/fibertodo/repos"
 	"github.com/RedGhoul/fibertodo/utils"
 	"github.com/gofiber/fiber"
 )
 
 func Show_Index(c *fiber.Ctx) {
-	users := repos.GetAllUsers()
-	utils.Render(c, "Home/index", "layouts/main", fiber.Map{"users": users,
-		"Title": "Hello, World!"})
+	utils.Render(c, "Home/index", "layouts/main", fiber.Map{
+		"Title": "Welcome to Fiber Snips"})
 }
